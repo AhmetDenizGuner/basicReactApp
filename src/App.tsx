498a9dd76ebab9f0d7b469979a3ea79285749616
Ahmet Deniz GÜNER <ahmetdenizguner@gmail.com>
2022-02-04 22:21:02 +0300

    routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from "./pages/Login";
+import Nav from "./components/Nav";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div className="App">
+        <BrowserRouter>
+        <Nav/>
       <main className="form-signin">
-        <form>
-            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-              <input type="email" className="form-control"  placeholder="Email address"/>
-              <input type="password" className="form-control"  placeholder="Password"/>
-            <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-        </form>
+              <Routes>
+                  <Route path="/" element={<Home/>}/>
+                  <Route path="/login" element={<Login/>} />
+                  <Route path="/register"  element={<Register/>} />
+              </Routes>
       </main>
+        </BrowserRouter>
     </div>
   );
 }
